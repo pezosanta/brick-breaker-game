884d43fb0e3658bef341a234f5999326a41ba2a8
htomi97 <hotalohru@gmail.com>
2020-05-07 00:01:21 +0200

    try commit

---
+++ src/asd.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+public class asd {
+    public static void main(String[] args) {
+        long myAge = 3_000_000_030L;
+        int herAge = 1;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(herAge);
+        Date now = new Date();
+        System.out.println(now);
+    }
+
+}
